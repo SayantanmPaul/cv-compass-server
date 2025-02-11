fe358994c54389b0604ea56df4fbd13b34ce7537
SayantanmPaul <sayantan.college6291@gmail.com>
2025-02-11 11:03:42 +0530

    fix: enable production mode

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 //load environment variables
 dotenv.config();
 
-const isProductionMode = false;
+const isProductionMode = true;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
